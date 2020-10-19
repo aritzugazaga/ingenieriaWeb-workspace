1c521490a530a139710f70c13568d34757ef5f23
Aritz Zugazaga <aritzugazaga@opendeusto.es>
2020-10-19 19:24:02 +0200

    Avance seccion de comentarios y presentación

--- Blog_AritzZugazaga/blog/App/models.py
+++ Blog_AritzZugazaga/blog/App/models.py
@@ -13,3 +13,12 @@ class App(models.Model):
 
     def get_absolute_url(self):
         return reverse('post_detail', args=[self.pk])
+
+class Comment(models.Model):
+    post = models.ForeignKey(App, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.App.title, self.name)
