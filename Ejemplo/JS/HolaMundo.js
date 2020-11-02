f45691437ccdf91c9d02042d47bf7ff7cf22e3cd
Aritz Zugazaga <aritzugazaga@opendeusto.es>
2020-11-02 09:49:34 +0100

    JS ejemplo

---
+++ Ejemplo/JS/HolaMundo.js
@@ -0,0 +1,12 @@
+var myStr1 = "Hola";
+var myStr2;
+myStr2 = ",";
+var myStr3 = "mundo!";
+//document.writeln(myStr1+myStr2);
+//document.writeln(myStr3);
+imprimir(myStr1+myStr2);
+imprimir(myStr3);
+
+function imprimir(arg) {
+    document.writeln(arg);
+}
